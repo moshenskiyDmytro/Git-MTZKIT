0c69451b96d85bdda394bc382faeba9e14146249
moshenskiyDmytro <moshenskyi.dmytro1122@vu.cdu.edu.ua>
2022-12-15 21:17:57 +0200

    Second commit

--- git_mtzkit/Program.cs
+++ git_mtzkit/Program.cs
@@ -7,6 +7,7 @@ namespace git_mtzkit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Second commit");
         }
     }
 }
